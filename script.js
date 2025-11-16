b49e936ebb7762797dabde445a94f2f6b52f28f6
987193 <jindalk198@gmail.com>
2025-11-16 23:34:04 +0530

    completed

---
+++ script.js
@@ -0,0 +1,108 @@
+const ad= document.querySelector('.song');
+const playing=document.querySelector('.fa-play');
+const pause=document.querySelector('.fa-pause');
+const forw=document.querySelector('.fa-forward-step');
+const ttl=document.querySelector('.title');
+const art_img=document.querySelector('#artist');
+const art_name=document.querySelector('#name');
+const playSong=document.querySelector('#playSong');
+
+
+const artist_name=['Talwiinder','The Weeknd','AP Dhillon','The Weeknd','Karan Aujla'];
+
+const artist_title=['Tu','Timeless','With You','Die for you','I Really Do'];
+
+playSong.addEventListener('click',effect);
+
+function effect(){
+    if(ad.duration==ad.currentTime){
+        x+=1;
+    }
+    if(!playing.classList.contains('none')){
+        ad.play();
+        setInterval(prog,1000);
+        setInterval(line,1000);
+        progress.addEventListener('click',(e)=>{
+            var widthbar2=(e.offsetX/e.target.clientWidth)*ad.duration;
+            ad.currentTime=widthbar2;
+        })
+    }
+    else {
+        ad.pause();
+    }
+    ttl.classList.toggle('run');
+    playing.classList.toggle('none');
+    pause.classList.toggle('none');
+    art_img.classList.toggle('round');
+    dur();
+}
+
+function removeEffect(){
+    ad.pause();
+    ad.currentTime=0.01;
+    ttl.classList.remove('run');
+    playing.classList.remove('none');
+    pause.classList.add('none');
+    art_img.classList.remove('round');
+}
+
+var x=0;
+function backward() {
+    dur();
+    x--;
+    if (x < 0) x = artist_name.length - 1;
+    removeEffect();
+    songs(x);
+}
+
+function forward() {
+    dur();
+    x++;
+    if (x >= artist_name.length) x = 0;
+    removeEffect();
+    songs(x);
+}
+
+
+function songs(x){
+    art_name.innerHTML=artist_name[x];
+    ttl.innerHTML=artist_title[x];
+    art_img.src=`./images/${x}.jpg`;
+    ad.src=`./songs/s${x}.mp3`;
+}
+songs(0);
+
+const lines = document.querySelector('.lineChild');
+const progress = document.querySelector('.line');
+const strt = document.querySelector('#start');
+const end =document.querySelector('#end');
+
+function dur(){
+    var dura=ad.duration;
+    var secdu=Math.floor(dura%60);
+    var mindu=Math.floor(dura/60);
+    if(secdu<10){
+        secdu=`0${secdu}`;
+    }
+    end.innerHTML=`${mindu}:${secdu}`
+}
+
+function prog(){
+    var currTime=ad.currentTime;
+    var mincur=Math.floor(currTime/60);
+    var seccur=Math.floor(currTime%60);
+    if(seccur<10){
+        seccur=`0${seccur}`;
+    }
+    strt.innerHTML=`${mincur}:${seccur}`;
+}
+
+function line(){
+    var widthbar=(ad.currentTime/ad.duration)*100;
+    lines.style.width=`${widthbar}%`;
+}
+const volume = document.querySelector('#volume');
+
+volume.addEventListener('input', ()=>{
+    ad.volume = volume.value;
+});
